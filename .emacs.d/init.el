72add74bdce9290b04c023890be0945e15944f5b
jmpargana <jmpargana@stud.uni-frankfurt.de>
2019-07-17 08:52:36 +0200

    optimizing sections full system config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (engine-mode try magit ox-reveal org-make-toc htmlize ox-gfm org-bullets markdown-mode company-auctex auctex tex intero haskell-mode company-tern json-mode prettier-js skewer-mode js2-refactor js2-mode emmet-mode web-mode flycheck-irony company-irony irony sphinx-doc company-jedi elpy move-text origami evil-nerd-commenter corral expand-region multiple-cursors dumb-jump flycheck company-quickhelp company autopair rainbow-delimiters powerline color-theme-sanityinc-tomorrow projectile dired-sidebar ace-window bash-completion counsel swiper ivy smex diminish use-package))))
+    (company-auctex engine-mode try magit ox-reveal org-make-toc htmlize ox-gfm org-bullets markdown-mode intero haskell-mode company-tern json-mode prettier-js skewer-mode js2-refactor js2-mode emmet-mode web-mode flycheck-irony company-irony irony sphinx-doc company-jedi elpy move-text origami evil-nerd-commenter corral expand-region multiple-cursors dumb-jump flycheck company-quickhelp company autopair rainbow-delimiters powerline color-theme-sanityinc-tomorrow projectile dired-sidebar ace-window bash-completion counsel swiper ivy smex diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
